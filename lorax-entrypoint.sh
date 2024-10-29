6368f34edc43fedbe0952caa453be1becd901aad
Ganesh varma Raghava raju <ganesh@Ganeshs-MacBook-Pro.local>
2024-10-29 21:05:54 +0530

    shebang

--- lorax-entrypoint.sh
+++ lorax-entrypoint.sh
@@ -1,4 +1,14 @@
+#!/bin/sh
 
 export PATH=/opt/conda/bin:$PATH
+export HUGGINGFACE_HUB_CACHE=/data 
+export HF_HUB_ENABLE_HF_TRANSFER=1
 
-nginx && lorax-launcher --json-output --model-id meta-llama/Meta-Llama-3.1-8B-Instruct 
+
+aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID && \
+aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY  && \
+aws configure set region us-east-1
+
+mkdir -p /data
+
+nginx && lorax-launcher --json-output --model-id TinyLlama/TinyLlama_v1.1
