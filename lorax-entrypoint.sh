6c565775168d15badca942908dee5d18691d58b3
Ganesh varma Raghava raju <ganesh@Ganeshs-MacBook-Pro.local>
2024-10-29 21:28:20 +0530

    shebang

--- lorax-entrypoint.sh
+++ lorax-entrypoint.sh
@@ -9,6 +9,9 @@ aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID && \
 aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY  && \
 aws configure set region us-east-1
 
+unset AWS_ACCESS_KEY_ID
+unset AWS_SECRET_ACCESS_KEY
+
 mkdir -p /data
 
 nginx && lorax-launcher --json-output --model-id TinyLlama/TinyLlama_v1.1
